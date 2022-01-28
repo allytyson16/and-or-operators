2e76fb2d7aef5bf9b25ddd0983de4d0be5cc09ae
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-01-28 10:24:37 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             Console.Write("Enter your age - ");
+		    int aggge = int.Parse(Console.ReadLine());
+		    Console.WriteLine("What is the movie rating? ");
+		    char rating = char.Parse(Console.ReadLine());
+		
+		    if(aggge <= 12 || aggge >= 65 && rating == 'G')
+		    {
+			Console.WriteLine("Discount applies");
+		    }       
         }
     }
 }
